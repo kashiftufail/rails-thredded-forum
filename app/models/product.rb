25d7c0d636e396705ae818fcb88f1b641bff3d6b
kashif tufail <kashif.tufail17@gmail.com>
2023-06-28 16:39:55 +0500

    searching with ransack and sorting too

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :user
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["body", "created_at", "id", "isbn", "price", "status", "title", "updated_at", "user_id"]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
 
   get "users/:id", to: "profiles#show"
   # Defines the root path route ("/")
-  # root "/forum"
+  root "products#index"
 end
